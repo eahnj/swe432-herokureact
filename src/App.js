d15ffc88838744c23c0679405eca886d8c376ba9
Abhilaash <abhilaash.velamati@gmail.com>
2020-04-25 09:39:01 -0400

    Moved fetch

--- src/App.js
+++ src/App.js
@@ -29,28 +29,6 @@ export const getLocationUrlData = () => {
 };
 
 
-const fetchData= async()=>{
-      if(bestLocation === "otherText") {
-        bestLocation = document.getElementById('otherText').value;
-      }
-      const res = await fetch(publicURL,
-        {
-          method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          mode: 'cors', // no-cors, *cors, same-origin
-          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          //credentials: 'same-origin', // include, *same-origin, omit
-          headers: {
-            // 'Content-Type': 'application/json'
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          //redirect: 'follow', // manual, *follow, error
-          //referrerPolicy: 'no-referrer', // no-referrer, *client
-          body  // body data type must match "Content-Type" header
-        }
-      );
-      const json = await res.json();
-      setResponse(json);
-    }
 //
 // function App(props) {
 //   const [weekDay, setWeekDay] = React.useState("Monday");
@@ -119,6 +97,29 @@ class App extends Component {
   };
 
 
+  const fetchData= async()=>{
+        if(bestLocation === "otherText") {
+          bestLocation = document.getElementById('otherText').value;
+        }
+        const res = await fetch(publicURL,
+          {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            //credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+              // 'Content-Type': 'application/json'
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            //redirect: 'follow', // manual, *follow, error
+            //referrerPolicy: 'no-referrer', // no-referrer, *client
+            body  // body data type must match "Content-Type" header
+          }
+        );
+        const json = await res.json();
+        setResponse(json);
+      }
+      
   render() {
     // const [bestPizza, serviceSpeedManhattan, serviceSpeedBlaze, bestLocation] = React.useState();
 
